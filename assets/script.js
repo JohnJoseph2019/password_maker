075d50f7e3ab354578e9c6280c9eda42719b797f
Juan Delgado <Delgado-juan@hotmail.com>
2023-10-20 16:34:23 -0500

    Create the begin function with a click event to higde the startpage

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,10 @@
 let startBtn = document.getElementById('start');
+let startPage = document.getElementById('firstPage');
 console.log(startBtn);
+
+function begin() {
+    //hide the first page
+    startPage.classList.add('hide');
+}
+
+startBtn.addEventListener('click', begin);
