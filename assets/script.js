e28e9b985615ccce8757c947becdbd8266683e4c
Juan Delgado <Delgado-juan@hotmail.com>
2023-10-27 17:43:43 -0500

    Here Finish displaying all criteria list using a fake module

--- assets/script.js
+++ assets/script.js
@@ -2,37 +2,62 @@ let startBtn = document.getElementById('start');
 let startPage = document.getElementById('firstPage');
 let promptPageEl = document.getElementById('prompt-page');
 let getInputElValue;
+let criteriaIndex = 0;
+let charactersList = {};
 
-function nextOption() {}
-function criteria() {
-    getInputElValue = parseInt(document.getElementById('lengthValue').value);
-    // console.log(getInputElValue);
-    if (isNaN(getInputElValue)) {
-        alert('Please type Number!!');
-        document.getElementById('lengthValue').value = '';
-    } else if (getInputElValue < 8 || getInputElValue > 128) {
-        alert('Please type a number between 8 - 128');
-        document.getElementById('lengthValue').value = '';
+let criteriaList = [
+    {
+        q: 'Do you want to have UPPERCASE Letters in your password ?',
+        id: 'UPPERCASE',
+    },
+    {
+        q: 'Do you want to have LOWERCASE Letters in your password ?',
+        id: 'LOWERCASE',
+    },
+    {
+        q: 'Do you want to have NUMBERS Letters in your password ?',
+        id: 'NUMBERS',
+    },
+    {
+        q: 'Do you want to have SPECIAL-CHARACTERS Letters in your password ?',
+        id: 'SPECIAL-CHARACTERS',
+    },
+];
+
+function nextOption(e) {
+    console.log(e.target.innerHTML);
+    if (e.target.innerHTML === 'YES') {
+        console.log('YES I WANT');
     } else {
-        //Ask for criteria
-        promptPageEl.innerHTML = '';
-        promptPageEl.style.width = '750px';
-        let criteriaQuestion = document.createElement('h2');
-        criteriaQuestion.textContent =
-            'Do you want to have UPPERCASE Letters in your password ?';
-        promptPageEl.appendChild(criteriaQuestion);
-        let yesButton = document.createElement('button');
-        yesButton.textContent = 'YES';
-        let noButton = document.createElement('button');
-        noButton.textContent = 'NO';
-        noButton.classList.add('btn');
-        yesButton.classList.add('btn');
-        promptPageEl.appendChild(yesButton);
-        promptPageEl.appendChild(noButton);
-        noButton.addEventListener('click', (e) => nextOption(e));
-        yesButton.addEventListener('click', (e) => nextOption(e));
+        console.log('NO I DO WANT');
+    }
+
+    if (criteriaIndex >= criteriaList.length - 1) {
+        console.log('Stop the criteria list');
+    } else {
+        criteriaIndex++;
+        askCriteria();
     }
 }
+function askCriteria() {
+    promptPageEl.innerHTML = '';
+    promptPageEl.style.width = '750px';
+    promptPageEl.style.height = 'fit-content';
+    let criteriaQuestion = document.createElement('h2');
+    criteriaQuestion.textContent = criteriaList[criteriaIndex].q;
+    promptPageEl.appendChild(criteriaQuestion);
+    let yesButton = document.createElement('button');
+    yesButton.textContent = 'YES';
+    let noButton = document.createElement('button');
+    noButton.textContent = 'NO';
+    noButton.classList.add('btn');
+    yesButton.classList.add('btn');
+    promptPageEl.appendChild(yesButton);
+    promptPageEl.appendChild(noButton);
+    noButton.addEventListener('click', (e) => nextOption(e));
+    yesButton.addEventListener('click', (e) => nextOption(e));
+}
+
 function askForLength() {
     // let box = document.createElement('div');
     promptPageEl.classList.add('lengthSection');
@@ -47,13 +72,29 @@ function askForLength() {
     promptPageEl.appendChild(inputElement);
     let submitLengthButton = document.createElement('button');
     submitLengthButton.textContent = 'Enter';
-    submitLengthButton.addEventListener('click', criteria);
+    submitLengthButton.addEventListener('click', () => {
+        getInputElValue = parseInt(
+            document.getElementById('lengthValue').value
+        );
+        // console.log(getInputElValue);
+        if (isNaN(getInputElValue)) {
+            alert('Please type Number!!');
+            document.getElementById('lengthValue').value = '';
+        } else if (getInputElValue < 8 || getInputElValue > 128) {
+            alert('Please type a number between 8 - 128');
+            document.getElementById('lengthValue').value = '';
+        } else {
+            //Ask for criteria
+            askCriteria();
+        }
+    });
     promptPageEl.appendChild(submitLengthButton);
     // promptPageEl.appendChild(box);
 }
 function begin() {
     //hide the first page
     startPage.classList.add('hide');
+    criteriaIndex = 0;
     askForLength();
 }
 
